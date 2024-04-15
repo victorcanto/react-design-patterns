cddaf5e285f0f62472c7fb60bf691f0ae3fe2156
Victor Canto <iamvictorcanto@gmail.com>
2024-04-15 10:12:46 -0300

    feat: add example demo of using the useTransition hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import { useDeferredValue, useState } from "react";
-import { HeavyComponent } from "./components/use-deferred-value/heavy-component";
+import { useState, useTransition } from "react";
+import Cover from "./components/use-transition/cover";
+import Reviews from "./components/use-transition/reviews";
+import Writer from "./components/use-transition/writer";
+import { StyledButton } from "./components/use-transition/styled-elements";
 
 function App() {
-	const [keyword, setKeyword] = useState("");
+  const [section, setSection] = useState("Cover");
+  const [isPending, startTransition] = useTransition();
 
-	const deferredKeyword = useDeferredValue(keyword);
+  const sectionHandler = (sec: string) => {
+    startTransition(() => {
+      setSection(sec);
+    });
+  };
 
-	console.log("Keyword:", keyword);
-	console.log("Deferred keyword:", deferredKeyword);
+  const renderSection = () => {
+    if (section === "Cover") {
+      return <Cover />;
+    }
+    if (section === "Reviews") {
+      return <Reviews />;
+    }
+    return <Writer />;
+  };
 
-	return (
-		<>
-			<input
-				type="text"
-				value={keyword}
-				onChange={(e) => setKeyword(e.target.value)}
-			/>
-			<HeavyComponent keyword={deferredKeyword} />
-		</>
-	);
+  return (
+    <>
+      <Button onClick={() => sectionHandler("Cover")}>Cover</Button>
+      <Button onClick={() => sectionHandler("Reviews")}>Book Reviews</Button>
+      <Button onClick={() => sectionHandler("Writer")}>Book's Writer</Button>
+      {isPending && <p>Loading...</p>}
+
+      {renderSection()}
+    </>
+  );
 }
+
+const Button = ({
+  onClick,
+  ...props
+}: JSX.IntrinsicAttributes & {
+  onClick: () => void;
+  children?: React.ReactNode;
+}) => {
+  return <StyledButton onClick={onClick} {...props} />;
+};
 
 export default App;
---
+++ src/components/use-transition/reviews.tsx
@@ -0,0 +1,25 @@
+import { ReviewsContainer } from "./styled-elements";
+
+const Reviews = () => {
+  return (
+    <ReviewsContainer>
+      <ul>
+        {Array(300)
+          .fill("")
+          .map((_, i) => (
+            <Review key={i} index={i} />
+          ))}
+      </ul>
+    </ReviewsContainer>
+  );
+};
+
+const Review = ({ index }: { index: number }) => {
+  const init = performance.now();
+  while (init > performance.now() - 3) {
+    // Fake slow down.
+  }
+  return <li>Review #{index}</li>;
+};
+
+export default Reviews;
