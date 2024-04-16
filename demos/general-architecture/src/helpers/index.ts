cae1944465cac239b0ae537342985bce11413e06
Victor Canto <iamvictorcanto@gmail.com>
2024-04-16 01:40:44 -0300

    refactor: rearrange files and folders from general-architecture

--- demos/general-architecture/src/helpers/index.ts
+++
@@ -1,3 +0,0 @@
-// Quaisquer utilitários e pequenas funções reutilizáveis devem entrar aqui.
-// Por exemplo, funções para formatar, data, hora, moeda e assim por diante.
-
---
+++ src/intl/index.ts
@@ -0,0 +1,3 @@
+// Diretorio relacionado com a internacialização, tambem conhecido como i18n. 
+// Devem ser adicionado utilitarios reutilizaveis para a internacionalização.
+// Por exemplo, funções para formatar, data, hora, moeda e assim por diante.
